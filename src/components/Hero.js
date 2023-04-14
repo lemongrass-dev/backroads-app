eaca31c1c9dc1b25c048a37f1536a08ac912aaac
Deepak <sayanthandeepak@gmail.com>
2023-04-14 12:46:21 +0530

    first commit

---
+++ src/components/Hero.js
@@ -0,0 +1,17 @@
+const Hero = () => {
+  return (
+    <section className="hero" id="home">
+      <div className="hero-banner">
+        <h1>backroads app</h1>
+        <p>
+          Wonder Full dream of my son Sayanthan Want to go World tour with him
+          and show him the world we cannot see
+        </p>
+        <a href="#tours" className="btn hero-btn ">
+          explore tours
+        </a>
+      </div>
+    </section>
+  )
+}
+export default Hero
